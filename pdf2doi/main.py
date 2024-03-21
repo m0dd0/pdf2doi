66b55eeb7797940d82bdc2c03b0ea469114e544f
Moritz Hesche <mo.hesche@gmail.com>
2024-03-21 16:49:39 +0100

    commenting out method 3 (document_text)

--- pdf2doi/main.py
+++ pdf2doi/main.py
@@ -164,10 +164,10 @@ def __find_doi(file: io.IOBase) -> dict:
         return result
 
     # Third method: We look in the plain text of the pdf and try to find something that matches a valid identifier.
-    logger.info(f"Method #3: Looking for a valid identifier in the document text...")
-    result = finders.find_identifier(file, method="document_text")
-    if result['identifier']:
-        return result
+    # logger.info(f"Method #3: Looking for a valid identifier in the document text...")
+    # result = finders.find_identifier(file, method="document_text")
+    # if result['identifier']:
+    #     return result
 
     # Fourth method: We look for possible titles of the paper, do a google search with them,
     # open the first results and look for identifiers in the plain text of the searcg results.
